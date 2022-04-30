f4c99d1d01056629332dcb1cd540808cc12fa002
ahmetfirat23 <69598652+ahmetfirat23@users.noreply.github.com>
2022-04-30 15:16:49 +0300

    Update ComicScene.cs

--- Assets/Scripts/MenuScripts/ComicScene.cs
+++ Assets/Scripts/MenuScripts/ComicScene.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+namespace ComicScene;
 
 public class ComicScene : MonoBehaviour
 {
